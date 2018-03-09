ca6ff9b6da950977c741f9ee8f4df82c67327838
lovehuyang90@163.com <lovehuyang90@163.com>
2018-03-09 18:11:40 +0800

    乱七八糟

---
+++ Swift/基本语法/第二部分/12-HLYWeibo/05-HLYWeibo按钮事件监听的副本/HLYWeibo/Classes/Home/HomeViewController.swift
@@ -0,0 +1,17 @@
+//
+//  HomeViewController.swift
+//  HLYWeibo
+//
+//  Created by tzsoft on 2018/2/27.
+//  Copyright © 2018年 胡小羊. All rights reserved.
+//
+
+import UIKit
+
+class HomeViewController: BaseViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        visitorView.addRotationAnim()
+    }
+}
