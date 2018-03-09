10351d5f0b2dbdca1ee4b5949eb0c3e9bc32959f
lovehuyang90@163.com <lovehuyang90@163.com>
2018-03-09 10:35:15 +0800

    遍历构造函数

--- Swift/基本语法/第二部分/12-HLYWeibo/03-HLYWeiboStoryBoard/HLYWeibo/Classes/Main/MainViewController.swift
+++ Swift/基本语法/第二部分/12-HLYWeibo/03-HLYWeiboStoryBoard/HLYWeibo/Classes/Main/MainViewController.swift
@@ -15,10 +15,57 @@ import UIKit
 
 class MainViewController: UITabBarController {
 
+    // MARK: - 懒加载
+   private lazy var imageNames = ["tabbar_message_center_selected","tabbar_message_center_selected","","tabbar_discover_selected","tabbar_profile_selected"]
+    //MARK:- 懒加载一个加号按钮
+    private lazy var composeBtn:UIButton = UIButton()
+    
+    
+    //MARK:- 系统回调函数
     override func viewDidLoad() {
         super.viewDidLoad()
-        
-        
+        setupComposeBtn()
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        setupTabbarItems()
     }
 }
 
+//MARK:- 初始化页面
+extension MainViewController{
+    /// 初始化加号按钮
+    private func setupComposeBtn(){
+        // 添加加号按钮
+        //1 将加号按钮添加到tabbar中
+        tabBar.addSubview(composeBtn)
+        //2 设置按钮属性
+        composeBtn.setBackgroundImage(UIImage.init(named: "tabbar_compose_button_os7"), for: .normal)
+        composeBtn.setBackgroundImage(UIImage.init(named: "tabbar_compose_button_highlighted_os7"), for: .selected)
+        composeBtn.setImage(UIImage.init(named: "tabbar_compose_icon_add_os7"), for: .normal)
+        composeBtn.setImage(UIImage.init(named: "tabbar_compose_icon_add_os7"), for: .selected)
+        
+        composeBtn.sizeToFit()
+        // 3 设置位置
+        composeBtn.center = CGPoint.init(x: tabBar.center.x, y: tabBar.bounds.size.height * 0.5)
+    }
+    
+
+    ///
+    private func setupTabbarItems(){
+        // 修改tabbar的属性
+        // 修改方法必须写在viewWillAppear中，若是写在viewDidLoad
+        //1 遍历所有的item
+        for (index , item) in (tabBar.items?.enumerated())! {
+            if(index == 2){
+                // 2 如果下标值为2，则item不可与用户交互
+                item.isEnabled = false
+                continue
+            }
+            
+            //3 设置其他的item选中的时候的图片
+            item.selectedImage = UIImage.init(named: imageNames[index])
+        }
+    }
+}
